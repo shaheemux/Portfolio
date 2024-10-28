aa41699501c2a3e8a782e78a675fa15155eb5cbd
Shaheem <Shaheemsalie349@gmail.com>
2024-10-28 10:17:01 +0200

    added files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -117,7 +117,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/1tbp02Nssd-KqDoDjX00hjZznto5fry2w/view?usp=sharing' target='blank_'
+              href='https://drive.google.com/file/d/13a8sFi2nO_SC4QuBNT9gDP5wgWnmGyh0/view?usp=sharing' target='blank_'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
--- src/data/data.js
+++ src/data/data.js
@@ -1,8 +1,11 @@
 import NetFlix from '../assets/projects/netflix-background-6eijmcazwfrb9u6w.jpg'
-import Portfolio from '../assets/projects/Portfolio.png'
 import Natures from '../assets/projects/nature.png'
 import VUE from '../assets/projects/Vue.png'
 import Elemental from '../assets/projects/Elemental.png'
+import Gallery from '../assets/projects/gallery.png'
+
+
+
 
 
 
@@ -15,31 +18,31 @@ export const data=[
         live:"https://netflix-clone-mu-jade.vercel.app/",
     },
     {
-        id:4,
+        id:2,
         name:"E-commerce/WordPress",
         image:Natures,
         github:"",
         live:"https://dev-natures-nibbles.pantheonsite.io/",
     },
     {
-        id:6,
+        id:3,
         name:"VUE.js Portfolio",
         image:VUE,
         github:"https://github.com/shaheemux/VuePortfolio.git",
         live:"https://vueportfolioemop.web.app",
     },
     {
-        id:7,
+        id:4,
         name:"Elemental",
         image:Elemental,
         github:"https://github.com/shaheemux/Elemental-challenge.git",
         live:"https://elemental-challenge.vercel.app/",
+    },
+    {
+        id:5,
+        name:"Gallery",
+        image:Gallery,
+        github:"https://github.com/shaheemux/Gallery.git",
+        live:"https://gallery-henna-five.vercel.app/",
     }
-    // {
-    //     id:7,
-    //     name:"Elemental",
-    //     image:Elemental,
-    //     github:"https://github.com/shaheemux/Elemental-challenge.git",
-    //     live:"https://elemental-challenge.vercel.app/",
-    // }
 ]
