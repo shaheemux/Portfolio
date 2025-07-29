79fc5628b8b004d73a372da70cc5aff42bb1345a
Shaheem <Shaheemsalie349@gmail.com>
2025-07-29 08:35:21 +0200

    added files

--- src/data/data.js
+++ src/data/data.js
@@ -1,8 +1,5 @@
-import NetFlix from '../assets/projects/netflix-background-6eijmcazwfrb9u6w.jpg'
 import Cred from '../assets/projects/cred.png'
-import VUE from '../assets/projects/Vue.png'
 import Elemental from '../assets/projects/Elemental.png'
-import Gallery from '../assets/projects/gallery.png'
 import Whatsapp from '../assets/projects/whatsapp.png'
 
 
@@ -11,46 +8,29 @@ import Whatsapp from '../assets/projects/whatsapp.png'
 
 
 export const data=[
+  
     {
         id:1,
-        name:"NetFlix Clone",
-        image:NetFlix,
-        github:"https://github.com/shaheemux/Netflix-Clone-.git",
-        live:"https://netflix-clone-mu-jade.vercel.app/",
-    },
-    {
-        id:2,
         name:"Agency UI Design",
         image:Cred,
         github:"",
         live:"https://creative-credentials.my.canva.site/",
     },
+ 
     {
-        id:3,
-        name:"VUE.js Portfolio",
-        image:VUE,
-        github:"https://github.com/shaheemux/VuePortfolio.git",
-        live:"https://vueportfolioemop.web.app",
-    },
-    {
-        id:4,
+        id:2,
         name:"Elemental",
         image:Elemental,
         github:"https://github.com/shaheemux/Elemental-challenge.git",
         live:"https://elemental-challenge.vercel.app/",
     },
     {
-        id:5,
-        name:"Gallery",
-        image:Gallery,
-        github:"https://github.com/shaheemux/Gallery.git",
-        live:"https://gallery-henna-five.vercel.app/",
-    },
-    {
-        id:6,
+        id:3,
         name:"Whatsapp Interface",
         image:Whatsapp,
         github:"https://github.com/shaheemux/Chef.git",
         live:"https://chef-red.vercel.app/",
-    }
+    },
+    
+
 ]
