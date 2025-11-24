1996ae3dcd75cbb4498727acb011f4a3fef25524
shaheem <shaheemsalie349@gmail.com>
2025-11-24 09:17:36 +0200

    added files

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,7 @@
 import Cred from '../assets/projects/cred.png'
 import Elemental from '../assets/projects/Elemental.png'
 import Whatsapp from '../assets/projects/whatsapp.png'
+import newspaper from '../assets/projects/newspaper.png'
 
 
 
@@ -30,6 +31,13 @@ export const data=[
         image:Whatsapp,
         github:"https://github.com/shaheemux/Chef.git",
         live:"https://chef-red.vercel.app/",
+    },
+    {
+        id:4,
+        name:"Newspaper ad",
+        image:newspaper,
+        github:"",
+        live:"https://drive.google.com/file/d/1e1jBPpCMCpEcAYOFT-Fz0LU5vsVvJje2/view?usp=sharing",
     },
     
 
